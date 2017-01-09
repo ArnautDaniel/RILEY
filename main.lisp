074b0a28af8a70d474a2b5d91cc022165a0fbc3b
Jack Lucas <silverbeard@protonmail.com>
2017-01-09 10:34:16 -0500

    Worked on latex generation

--- main.lisp
+++ main.lisp
@@ -414,11 +414,6 @@ col-md-push-1"
 				     :name "invoice" :id "invoice")
 			     (:button :type "submit" :class "btn btn-default btn-sm btn-danger" "Remove")))))))))))
 			     
-		      
-(defmacro test-left-side ()
-  `(with-html-output (*standard-output* nil :indent t)
-     (:div :class "col-md-6"
-	   (:h1 "Hello world"))))
 (defmacro standard-picture-upload ()
   `(with-html-output (*standard-output* nil :indent t)
      (:div :class "panel panel-default col-md-6 col-md-push-1"
@@ -446,7 +441,9 @@ col-md-push-1"
 		      (:li :class "list-group-item list-group-item-success" ,show)
 		      (:li :class "list-group-item list-group-item-danger" ,set)
 		      (:li :class "list-group-item list-group-item-success" ,contact)
-		      (:li :class "list-group-item list-group-item-danger" ,pic-num))))))
+		      (:li :class "list-group-item list-group-item-danger" ,pic-num))
+		 (:a :href "/createpdf" (:button :type "submit" :class "btn btn-default btn-sm btn-info"
+						 "Create PDF"))))))
 
 ;;;Define page handler functions
 
@@ -574,6 +571,12 @@ col-md-push-1"
   (mapcar #'(lambda (x)
 	      (item-picture x))
 	  (invoice-item-list invoice)))
+
+(define-easy-handler (createpdf :uri "/createpdf") ()
+  (let* ((invoice (find-invoice-from-cookie (cookie-in "current-invoice")))
+	 (root-dir (invoice-root-dir invoice)))
+    (generate-latex invoice root-dir))
+  (redirect "/setthemcookies"))
 
 ;;;Need to rewrite so defintions are in the URL for reloading to work
 (define-easy-handler (setthemcookies :uri "/setthemcookies") ()
@@ -721,3 +724,102 @@ col-md-push-1"
 		 (:center (:button :type "submit"
 			  :class "btn btn-primary"
 			  "Login"))))))))))))
+
+
+;;;Latex constants---------------------------------------------------
+
+(defparameter header-name "\\dotfill \\textsc{Ordered by:} ")
+(defparameter header-set "\\dotfill \\textsc{Set:} ")
+(defparameter header-show "\\dotfill \\textsc{Show:} ")
+(defparameter date-header "\\tab {\\bf Pickup Date:} \\today \\dotfill")
+(defparameter rental-period "{\\bf {Rental Period:}")
+(defparameter begin-table "\\begin{invoiceTable}")
+(defparameter end-table "\\end{invoiceTable}")
+(defparameter unitrow "\\unitrow{")
+
+;;;Takes an invoice and root-dir
+
+(defparameter tail-conf "
+\\par
+{\\scriptsize \\begin{singlespace} It is agreed that \\textsc{Lessee} assumes all liability and responsibility for the item(s) listed above.  When item(s) have been accepted by \\textsc{Lessee} (as evidenced by \\textsc{Lessee’s} signature below), and while in the custody and possession of \\textsc{Lessee}, its employees, agents, owners, directors and officers and any other person or entity to whom the item(s) is entrusted by \\textsc{Lessee}, or assigns.  Liability extends to the full replacement cost or repair, in Lessor’s sole discretion.  Further, Lessee assumes full responsibility for any liability arising because of the use of the item(s) during the term of the lease and until item(s) is returned to the custody and control of Lessor (as evidenced by the written acknowledgement by Lessor). Further, to the extent permitted by law, the \\textsc{Lessee} agrees to protect, indemnify, defend and hold harmless Lessor, its directors, officers, agents, shareholders, and employees, against all claims or damages to people or property and costs (including reasonable attorney’s fees), up to Lessor’s pro-rata share of the item(s), arising out of or connected with the operation of \\textsc{Lessee’s} activities with respect to the item(s), including damage caused by inadequate maintenance of the item(s), use of the item(s), or a part thereof, by any customer, any guest, invitee, or by any agent of the \\textsc{Lessee}. Replacement value of all items (unless otherwise noted) equals (10X) first week rate. \\end{singlespace}}
+
+{\\color{red} \\textsc{Signature}}\\hspace{0.5cm} \\makebox[3in]{\\hrulefill} \\hspace{0.5cm} \\textsc{Date}\\hspace{0.5cm} \\makebox[1in]{\\hrulefill} \\\\
+\\textsc{Print}\\hspace{1.25cm} \\makebox[3in]{\\hrulefill}
+\\newpage
+\\end{document}")
+
+
+(defparameter document-conf "\\documentclass{invoice} % Use the custom invoice class (invoice.cls)
+
+\\def \\tab {\\hspace*{3ex}} % Define \\tab to create some horizontal white space
+
+\\usepackage{color}
+\\usepackage{courier}
+\\usepackage{setspace}
+\\usepackage{graphicx}
+\\usepackage{subfig}
+\\usepackage{pgffor}
+\\usepackage{caption}
+\\usepackage{expl3}
+
+
+\\begin{document}")
+
+(defparameter heading-conf "
+\\hfil{\\huge\\color{red}{\\textsc{Checkout Sheet}}}\\hfil
+% \\bigskip\\break % Whitespace
+\\break
+\\hrule % Horizontal line
+
+000 E Atlanta Drive \\hfill \\emph{Mobile:} (000) 000-0000 \\\\
+Ste. 000 \\hfill{ \\emph{Office:} (000) 000-0000} \\\\
+% Your address and contact information
+Norfolk, Georgia 00000 \\hfill anon@anon.com
+\\\\ \\\\
+{\\bf Invoice To:} \\\\ % From here --->")
+
+;;;Latex Functions----------------------------------------------
+(defun generate-invoice-single-header (invoice func)
+  (concatenate 'string  func invoice))
+
+(defun generate-latex (invoice root-dir)
+  (ensure-directories-exist (concatenate 'string root-dir
+					 "pdf/"))
+  (let* ((pdfname (concatenate 'string "pdf/" (show-name invoice)
+			       "-" (invoice-set-name invoice)
+			       ".tex"))
+	 (complete-stream (concatenate 'string root-dir
+				       pdfname)))
+    (with-open-file (s complete-stream :direction :output
+		       :if-exists :supersede)
+      (princ document-conf s)
+      (fresh-line s)
+      (princ heading-conf s)
+      (princ (generate-invoice-single-header
+	      (show-name invoice) header-show) s)
+      (princ (generate-invoice-single-header
+	      (invoice-set-name invoice) header-set) s)
+      (princ (generate-invoice-single-header
+	      (invoice-contact-name invoice) header-name) s)
+      (fresh-line s)
+      (fresh-line s)
+      (princ date-header s)
+      (princ rental-period s)
+      (fresh-line s)
+      (princ begin-table s)
+      (fresh-line s)
+      (mapc (lambda (b) (princ (format-description b) s)
+		    (fresh-line s)) (invoice-item-list invoice))
+      (princ end-table s)
+      (fresh-line s)
+      (princ tail-conf s))))
+
+(defun format-description (b)
+  (concatenate 'string
+	       unitrow
+	       (item-description b)
+	       "}{"
+	       (item-quantity b) "}{"
+	       (item-price b) "}{}"))
+    
+	 
