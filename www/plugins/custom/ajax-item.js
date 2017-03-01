2ee552c5fd0460f3981b77044330484cc92400e3
Jack Lucas <silverbeard@protonmail.com>
2017-03-01 11:57:48 -0500

    If I'm lucky future employers wont look at my javascript

--- www/plugins/custom/ajax-item.js
+++ www/plugins/custom/ajax-item.js
@@ -1,5 +1,4 @@
 $(function() { var form = $('#new-item');
-var formMessages = $('#form-messages');
 $(form).submit(function(event) { event.preventDefault();
 var desc = document.getElementById('input-item-description').value;
 var price = document.getElementById('input-item-price').value;
@@ -22,12 +21,15 @@ var cell2 = row.insertCell(1);
 var cell3 = row.insertCell(2);
 var cell4 = row.insertCell(3);
 cell1.innerHTML = desc;
-cell2.innerHTML = price;
+cell2.innerHTML = price; 
 cell3.innerHTML = qty;
 var newimg = $("#image-name");
-$("#input-picture").removeAttr("src").attr("src", newimg.src);
 
-
+if ($("#image-name").attr("value") == null) {
+$("#input-picture").replaceWith("<p> All out of pictures </p>"); }
+else {
+$("#input-picture").replaceWith("<img id='input-picture' src='" + $("#image-name").attr("value") + "' class='img-responsive' width='40%' height='40%' name='" + $("#image-name").attr("value") + "' />");
+$('#modalImg').find('form').first().remove();}
 },
 error: function() {
 alert('error');
